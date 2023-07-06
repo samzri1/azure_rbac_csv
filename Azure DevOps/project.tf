6bf99b07f0162a25b4cf2fe1bbcd0d451ad803d8
samzri1 <109951209+samzri1@users.noreply.github.com>
2023-07-06 15:18:07 +0200

    Update project.tf

--- Azure DevOps/project.tf
+++ Azure DevOps/project.tf
@@ -12,8 +12,7 @@ provider "azuredevops" {
 }
 
 locals {
-  # name       = ["Aley", "Aïcha", "Alain", "Bouba", "Bilal", "Clément", "Driton", "Khaled", "Salem", "Seif", "Yassine", "El-mahdi"]
-  input = jsondecode(file("./principals.json"))
+   input = jsondecode(file("./principals.json"))
   principals = {for i in local.input : i.id => i}
 }
 
@@ -21,7 +20,7 @@ output "principal" {
   value = local.principals
 }
 
-module "p20_module" {
+module "p22_module" {
   source = "./modules/p20_create_azdo_projects"
   for_each = local.principals
   mail = each.key
